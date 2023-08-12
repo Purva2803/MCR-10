52c262a40f93f43b93ee57b1e244b449cbe1de3e
Purva2803 <rajyagurupurva28@gmail.com>
2023-08-12 21:41:24 +0530

    fixed the issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Departments } from './components/departments'
-import { Products } from './components/products'
-import { Dashboard } from './components/dashboard'
-import { Routes,NavLink,Route } from 'react-router-dom'
-import '../src/styles/sidebar.css'
-import { AddProduct } from '../src/pages/AddProduct'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Departments } from "./components/departments";
+import { Products } from "./components/products";
+import { Dashboard } from "./components/dashboard";
+import { Routes, NavLink, Route } from "react-router-dom";
+import "../src/styles/sidebar.css";
+import { AddProduct } from "../src/pages/AddProduct";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <div className="sidebar">
-          
-        <div className='navlink'>
-          <NavLink to="/" activeClassName="active">Dashboard</NavLink>
-          <NavLink to="/departments" activeClassName="active">Departments</NavLink>
-          <NavLink to="/products" activeClassName="active">Products</NavLink>
-
-        </div>
+          <div className="navlink">
+            <NavLink to="/" activeClassName="active">
+              Dashboard
+            </NavLink>
+            <NavLink to="/departments" activeClassName="active">
+              Departments
+            </NavLink>
+            <NavLink to="/products" activeClassName="active">
+              Products
+            </NavLink>
+          </div>
         </div>
 
         <div>
           <Routes>
-            <Route path="/" element={<Dashboard/>}/>
-            <Route path="/departments" element={<Departments/>}/>
-            <Route path="/products" element={<Products/>}/>
-            <Route path='/addProduct' element={<AddProduct/>}/>
-            <Route path= {`/departments/${Kitchen}`} element={<Products/>}/>
-            <Route path= {`/departments/${Toys}`} element={<Products/>}/>
-            <Route path= {`/departments/${Clothing}`} element={<Products/>}/>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/departments" element={<Departments />} />
+            <Route path="/products" element={<Products />} />
+
+            <Route path="/addProduct" element={<AddProduct />} />
+
+            <Route path="/departments/:department" element={<Products />} />
           </Routes>
         </div>
-
-        </div>
-
-       
-
-        
-
-           
-
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
